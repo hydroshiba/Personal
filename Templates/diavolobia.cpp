8e2a790b3def31d5150c4ed5eda4a658937cf25d
Mark Phan <36961737+hydroshiba@users.noreply.github.com>
2021-04-13 12:47:19 +0700

    Add files via upload

---
+++ Templates/diavolobia.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void king_crimson(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(null);
+    cin.tie(null); cout.tie(null);
+
+    int enemies;
+    cin >> enemies;
+
+    while(--enemies){
+        king_crimson();
+    }
+}
